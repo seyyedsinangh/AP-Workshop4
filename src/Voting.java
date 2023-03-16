3f7fbac096b27aa23d3e7c61e523bb7f00e60468
farbod <farbod.bjary82@gmail.com>
2023-03-16 14:14:31 +0330

    getters, setters, createChoices, printVotes and equals methods implemented

--- src/Voting.java
+++ src/Voting.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Voting {
     private int type;
@@ -25,6 +23,29 @@ public class Voting {
 
     public HashMap<String, HashSet<Vote>> getChoices() {
         return choices;
+    }
+
+    public void createChoices(String choice) {
+        HashSet<Vote> votes = new HashSet<Vote>();
+        this.choices.put(choice, votes);
+    }
+
+    public void printVotes() {
+        for(String choice: this.choices.keySet()){
+            System.out.println(choice);
+            for(Vote voter: this.choices.get(choice)){
+                System.out.println("first name: " + voter.getVoter().getFirstName() + "\n" + "last name: " + voter.getVoter().getLastName());
+                System.out.println("date of vote: " + voter.getDate());
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Voting voting = (Voting) o;
+        return type == voting.type && isAnonymous == voting.isAnonymous && question.equals(voting.question) && Objects.equals(voters, voting.voters) && choices.equals(voting.choices);
     }
 
 }
